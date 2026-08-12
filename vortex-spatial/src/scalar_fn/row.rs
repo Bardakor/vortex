2c24de405c117fbdc741860edad51f1567834aae
Connor Tsui <connor.tsui20@gmail.com>
2026-08-12 13:39:38 -0400

    Opt spatial distance into unstable RowFn
    Signed-off-by: Connor Tsui <connor.tsui20@gmail.com>

--- vortex-spatial/src/scalar_fn/row.rs
+++ vortex-spatial/src/scalar_fn/row.rs
@@ -8,7 +8,7 @@ use geo_types::Geometry;
 use vortex_array::ArrayRef;
 use vortex_array::ExecutionCtx;
 use vortex_array::dtype::DType;
-use vortex_array::scalar_fn::InputElement;
+use vortex_array::scalar_fn::unstable::row::InputElement;
 use vortex_error::VortexResult;
 use vortex_error::vortex_ensure;
 
